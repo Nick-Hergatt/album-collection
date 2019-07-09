5aaf0b433f5b53db4526ba0663a9f908b4544913
Isaacjk614 <isaacjk614@gmail.com>
2019-07-09 15:13:54 -0400

    album weblayer, controller jpa tests

---
+++ src/main/java/wcci/albumcollection/AlbumController.java
@@ -0,0 +1,25 @@
+package wcci.albumcollection;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping({"/api","/api/"}) //If something breaks, this may be the problem
+public class AlbumController {
+	@Autowired
+	AlbumRepository albumRepo;
+	
+	@RequestMapping("/albums")
+	public Iterable<Album> sendAlbums(){
+		return albumRepo.findAll();
+	}
+	
+	@GetMapping ("/albums/{id}")
+	public Album sendAlbum(@PathVariable Long id) {
+		return albumRepo.findById(id).get();
+	}
+
+}

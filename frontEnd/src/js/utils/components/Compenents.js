979073b7129b27314540f90cfa9c3de182fd78b7
bryce-sampson <sampsonbrycea@gmail.com>
2019-07-23 12:00:29 -0400

    finish writing out component renderings

--- frontEnd/src/js/utils/components/Compenents.js
+++ frontEnd/src/js/utils/components/Compenents.js
@@ -43,44 +43,100 @@ class Components {
             event.preventDefault();
             this.renderPageArtists();
           })
-      );   
+      );
   }
-  renderMainFooter(){
-    const mainFooter = html().create('footer').addClass('main-footer');
-    const mainFooterCopy = html().create('small').addClass("main-footer__copy").addHtml('&copy; 2019 WildGroove');
+  renderMainFooter() {
+    const mainFooter = html()
+      .create("footer")
+      .addClass("main-footer");
+    const mainFooterCopy = html()
+      .create("small")
+      .addClass("main-footer__copy")
+      .addHtml("&copy; 2019 WildGroove");
     mainFooter.addChild(mainFooterCopy);
     return mainFooter;
   }
-  rendnerContentBlock(requestedData){
-    const contentBlock = html().create('section').addClass('content-block');
-    const contentBlockTitle = html().create('h3').addClass('content-block__title').text(requestedData);
-    const contentBlockList = html().create('ul').addClass('content-block__list');
-    Api().getRequest(`http://localhost:8080/api/${requestedData}`, (responseCollection) => {
-      responseCollection.forEach((item) => {
-       
-     let elementName
-        if (item.albumTitle){
-          elementName = item.albumTitle
-        } else if (item.name){
-          elementName = item.name 
-        }
-        else{
-          elementName = item.songTitle
-        }
-        const contentBlockListItem = html()
-          .create('li')
-          .addClass('content-block__list-item')
-          .addChild(html()
-            .create('a')
-            .addAttribute('href', '/${requestedData}/${item.id}')
-            .text(elementName));
+  renderContentBlock(requestedData) {
+    const contentBlock = html()
+      .create("section")
+      .addClass("content-block");
+    const contentBlockTitle = html()
+      .create("h3")
+      .addClass("content-block__title")
+      .text(requestedData);
+    const contentBlockList = html()
+      .create("ul")
+      .addClass("content-block__list");
+    Api().getRequest(
+      `http://localhost:8080/api/${requestedData}`,
+      responseCollection => {
+        responseCollection.forEach(item => {
+          let elementName;
+          if (item.albumTitle) {
+            elementName = item.albumTitle;
+          } else if (item.name) {
+            elementName = item.name;
+          } else {
+            elementName = item.songTitle;
+          }
+          const contentBlockListItem = html()
+            .create("li")
+            .addClass("content-block__list-item")
+            .addChild(
+              html()
+                .create("a")
+                .addAttribute("href", "/${requestedData}/${item.id}")
+                .text(elementName)
+            );
 
-        contentBlockList.addChild(contentBlockListItem);
-    });
-  });
-  contentBlock.addChild(contentBlockTitle);
-  contentBlock.addChild(contentBlockList);
-  return contentBlock;
-}
-}
+          contentBlockList.addChild(contentBlockListItem);
+        });
+      }
+    );
+    contentBlock.addChild(contentBlockTitle);
+    contentBlock.addChild(contentBlockList);
+    return contentBlock;
+  }
+  renderMainContent(requestedData) {
+    const mainContent = html()
+      .create("main")
+      .addClass("main-content");
+    const containerDiv = html()
+      .create("div")
+      .addClass("container");
+    const contentBlock = this.renderContentBlock(requestedData);
+    containerDiv.addChild(contentBlock);
+    mainContent.addChild(containerDiv);
+    return mainContent;
+  }
+  renderPageArtists() {
+    const currentMainContentContainer = this.getWrapperDiv()
+      .select(".main-content")
+      .select(".container");
+    currentMainContentContainer.replace(this.renderContentBlock("artists"));
+  }
+  renderPageAlbums() {
+    const currentMainContentContainer = this.getWrapperDiv()
+      .select(".main-content")
+      .select(".container");
+    currentMainContentContainer.replace(this.renderContentBlock("albums"));
+  }
+  renderPageSongs() {
+    const currentMainContentContainer = this.getWrapperDiv()
+      .select(".main-content")
+      .select(".container");
+    currentMainContentContainer.replace(this.renderContentBlock("songs"));
+  }
 
+  renderPageHome() {
+    const app = this.getAppContext();
+    const wrapperDiv = this.getWrapperDiv();
+    const mainHeader = this.renderMainHeader();
+    const mainContent = this.mainContent("artists");
+    const mainFooter = this.mainFooter();
+    wrapperDiv.addChild(mainHeader);
+    wrapperDiv.addChild(mainContent);
+    wrapperDiv.addChild(mainFooter);
+    app.replace(wrapperDiv);
+  }
+}
